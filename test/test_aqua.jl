344fd0b69e5100962fbe017eb156094c1890bc5b
Oliver Schulz <oschulz@mpp.mpg.de>
2023-11-25 22:30:51 +0100

    Update Aqua config

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -11,7 +11,6 @@ end # testset
 Test.@testset "Aqua tests" begin
     Aqua.test_all(
         JuliaPackageTemplate,
-        ambiguities = true,
-        project_toml_formatting = VERSION≥v"1.7"
+        ambiguities = true
     )
 end # testset
